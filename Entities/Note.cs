71975bb94ed0f69d53fb11028bdc29c261ee3e47
egeckmk <egeckmk@gmail.com>
2022-04-05 23:26:38 +0300

    Created project architecture. Web API will be added for the project.

---
+++ Entities/Note.cs
@@ -0,0 +1,17 @@
+﻿using InessNote.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InessNote.Domain.Entities
+{
+  public class Note : BaseEntity
+  {
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public string Author { get; set; }
+    public List<string> Tags { get; set; }
+  }
+}
